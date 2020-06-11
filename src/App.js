ae9dec95389905cbffe46c970351479e0acceef0
accordingtoherr <emilyherr94@gmail.com>
2020-06-11 00:39:20 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -2,29 +2,28 @@ import React, { useEffect, useState, Component } from 'react'
 // get our fontawesome imports
 
 
-import Cat from './Cat.js'
+import Cat from './Cat.js';
 import './App.css'
 
 
 const App = () => {
  
-  
+  const [cats, setCats] = useState([]);
 
   useEffect(() => {
- 
+    getCats();
   }, [])
 
-  const getSubs = async () => {
+  const getCats = async () => {
     const response = await fetch(`https://api.thecatapi.com/v1/images/search`)
-   
+
 
     const data = await response.json();
     console.log(data.url);
 
-  
+    setCats(data.url);
     // setSubs(prevSubs => prevSubs.concat(data.data.children)) // Use this setState with function argument if you want to concat API response with previous state data
   }
-
 
 
 
@@ -33,10 +32,10 @@ const App = () => {
       <h1 className="header">
      
       </h1>
-
-    <Cat />
     
         <button type="submit" className="button">New Cat</button>
+
+        <Cat />
 
 
          
